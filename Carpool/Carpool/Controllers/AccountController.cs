2d4a307a5c2fbe7b823f1283c24b63aab4d95a9b
Nathanaël <nathanael.omnes@gmail.com>
2018-02-14 12:55:59 +0000

    Disabled js validators for tests

--- Carpool/Carpool/Controllers/AccountController.cs
+++ Carpool/Carpool/Controllers/AccountController.cs
@@ -53,7 +53,9 @@ namespace Carpool.Controllers
         {
             List<string> errorsList = new List<string>();
 
-            //if(test compulsory fields empty)
+            if (pUser.UserName == null || pUser.FirstName == null || pUser.LastName == null || pUser.Password == null)
+                //Create check password 2 times
+                errorsList.Add("One compulsory field or more are empty.");
 
             if (errorsList.Any())
             {
