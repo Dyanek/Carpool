a886f707ff8966a4b3dc52347a5fd09d77531604
Dyanek <nathanael.omnes@gmail.com>
2018-03-02 18:53:14 +0000

    Trip creation in progress

--- Carpool/Carpool/Controllers/TripController.cs
+++ Carpool/Carpool/Controllers/TripController.cs
@@ -20,19 +20,98 @@ namespace Carpool.Controllers
         [HttpGet]
         public ActionResult CreateTrip()
         {
+            Trip trip = new Trip()
+            {
+                Beginning = DateTime.Now,
+                Closing = DateTime.Now.AddMinutes(-1)
+            };
+
             if (!UserIsConnected())
                 return RedirectToAction("Index", "Home");
 
-            return View();
+            ViewBag.Days = new SelectList(Enumerable.Range(1, 31));
+            ViewBag.Months = new SelectList(Enumerable.Range(1, 12));
+            ViewBag.Years = new SelectList(Enumerable.Range(2018, 50));
+            ViewBag.Hours = new SelectList(Enumerable.Range(0, 24));
+            ViewBag.Minutes = new SelectList(Enumerable.Range(0, 60));
+            ViewBag.CountriesList = new SelectList(DbContext.Countries.Where(x => x.Name != null).ToList(), "Id", "Name");
+
+            return View(trip);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreateTrip(Trip pTrip)
+        public ActionResult CreateTrip(Trip pTrip, string price, string pBeginning)
         {
-            pTrip.IsRoutine = false;
+            pTrip.Price = Convert.ToDecimal(price);
 
-            return RedirectToAction("ManageTrips");
+            if (!UserIsConnected())
+                return RedirectToAction("Index", "Home");
+
+            List<string> errorsList = new List<string>();
+
+            if (pTrip.Beginning == null || pTrip.Closing == null || pTrip.Address.Line1 == null || pTrip.Address.PostalCode == null || pTrip.Address.City.Name == null)
+                errorsList.Add("One compulsory field or more are empty.");
+
+            if (pTrip.NumberOfPlaces <= 0 || pTrip.NumberOfPlaces > 10)
+                errorsList.Add("The number of places must be between 1 and 10");
+
+            if (pTrip.Price <= 0)
+                errorsList.Add("The price must be positive, the decimals must be separated with a coma");
+
+            if (pTrip.Duration <= 0)
+                errorsList.Add("The duration must be positive");
+
+            if (pTrip.Beginning <= DateTime.Now || pTrip.Closing <= DateTime.Now)
+                errorsList.Add("The beginning and closing dates must be over " + DateTime.Now);
+
+            if (pTrip.Beginning < pTrip.Closing)
+                errorsList.Add("The closing date must be before the beginning date");
+
+            if (errorsList.Any())
+            {
+                TempData["Error"] = ConcatenateErrors(errorsList);
+
+                ViewBag.Days = new SelectList(Enumerable.Range(1, 31));
+                ViewBag.Months = new SelectList(Enumerable.Range(1, 12));
+                ViewBag.Years = new SelectList(Enumerable.Range(2018, 50));
+                ViewBag.Hours = new SelectList(Enumerable.Range(0, 24));
+                ViewBag.Minutes = new SelectList(Enumerable.Range(0, 60));
+                ViewBag.CountriesList = new SelectList(DbContext.Countries.Where(x => x.Name != null).ToList(), "Id", "Name");
+
+                return View();
+            }
+            else
+            {
+                pTrip.IsRoutine = false;
+
+                pTrip.Address.City = CheckCity(pTrip.Address.City.Name, pTrip.Address.City.Country.Id);
+
+                pTrip.Address = CheckAddress(pTrip.Address);
+
+                try
+                {
+                    DbContext.Trips.Add(pTrip);
+                    DbContext.SaveChanges();
+                }
+                catch(Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+
+                Joins join = new Joins()
+                {
+                    Date = DateTime.Now,
+                    UserId = ConnectedUser.Id,
+                    TripId = DbContext.Trips.Last().Id
+                };
+
+                DbContext.Joins.Add(join);
+                DbContext.SaveChanges();
+
+                TempData["Success"] = "You have succesfully created the trip";
+                return RedirectToAction("ManageTrips");
+            }
         }
 
         public ActionResult FindTrips()
--- Carpool/Carpool/Models/_Trip.cs
+++ Carpool/Carpool/Models/_Trip.cs
@@ -9,21 +9,44 @@ namespace Carpool.Models
     [MetadataType(typeof(TripMetadata))]
     public partial class Trip
     {
+        public Trip(DateTime beginningDate, DateTime closingDate)
+        {
+            Beginning = beginningDate;
+            Closing = closingDate;
+        }
+
         private class TripMetadata
         {
             [Display(Name = "Number of places")]
             [Required(ErrorMessage = "You need to enter a valid number")]
+            [Range(1, 10, ErrorMessage = "The number of places must be between 1 and 10")]         
             public int NumberOfPlaces { get; set; }
 
+            [Required(ErrorMessage = "You need to enter a valid price")]
+            [Range(0.1, 10000, ErrorMessage = "The price must be between £0.10 and £10000")]
+            public decimal Price { get; set; }
+
+            [Required(ErrorMessage = "You must enter a duration")]
+            [Range(1, 1440, ErrorMessage = "The duration must be between 1 and 1440 minutes")]          
+            public DateTime Duration { get; set; }
+
             [Display(Name = "Beginning date")]
-            [Required(ErrorMessage = "You must enter a valid date, the format is MM/dd/yyyy hh:mm")]
-            [RegularExpression(@"^[0-1]\d\/[0-3]\d\/[2][0]\d{2}\s[0-2]\d:[0-5]\d$", ErrorMessage = "Date must be in format MM/dd/yyyy hh:mm")]
+            //[Required(ErrorMessage = "You must enter a valid date, the format is dd/MM/yyyy hh:mm")]
+            //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+            //[RegularExpression(@"^[0-3]\d\/[0-1]\d\/[2][0]\d{2}\s[0-2]\d:[0-5]\d$", ErrorMessage = "Date must be in format dd/MM/yyyy hh:mm")]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
             public DateTime Beginning { get; set; }
 
             [Display(Name = "Closing date")]
-            [Required(ErrorMessage = "You must enter a valid date, the format is MM/dd/yyyy hh:mm")]
-            [RegularExpression(@"^[0-1]\d\/[0-3]\d\/[2][0]\d{2}\s[0-2]\d:[0-5]\d$", ErrorMessage = "Date must be in format MM/dd/yyyy hh:mm")]
+            //[Required(ErrorMessage = "You must enter a valid date, the format is dd/MM/yyyy hh:mm")]
+            //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+            //[RegularExpression(@"^[0-3]\d\/[0-1]\d\/[2][0]\d{2}\s[0-2]\d:[0-5]\d$", ErrorMessage = "Date must be in format dd/MM/yyyy hh:mm")]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
             public DateTime Closing { get; set; }
+
+
         }
     }
 }
